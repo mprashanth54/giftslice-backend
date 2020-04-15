f4f270c64cacdde67fd5126cf35b273c945dfd33
Prashanth Mogali <m.prashanth54@gmail.com>
2020-04-15 01:25:49 -0400

    Prashanth | Gifts global retreive

--- controllers/gift.js
+++ controllers/gift.js
@@ -2,16 +2,16 @@ const gift = require('express').Router()
 const GiftService = require('../services/gift')
 // const CampaignService = require('../services/campaign')
 
-gift.get('/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const { userID } = req.user.id
-        const gift = await GiftService.getById(id, userID)
-        res.json(gift)
-    } catch (err) {
-        res.status(400).json({ error: err })
-    }
-})
+// gift.get('/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params
+//         const { userID } = req.user.id
+//         const gift = await GiftService.getById(id, userID)
+//         res.json(gift)
+//     } catch (err) {
+//         res.status(400).json({ error: err })
+//     }
+// })
 
 gift.post('/', async (req, res) => {
     try {
@@ -31,6 +31,16 @@ gift.post('/global', async (req, res) => {
         res.status(400).json({ error: err })
     }
 })
+
+gift.get('/global', async (req, res) => {
+    try {
+        const gifts = await GiftService.getGlobal()
+        res.json({ gifts: gifts })
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+})
+
 
 
 module.exports = gift
