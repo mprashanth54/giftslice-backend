9c5e9b01cffdedd5a095e14930bda604e6a4fc87
Prashanth Mogali <prashanthmogali@Prashanths-MacBook-Pro.local>
2020-04-30 00:16:38 -0400

    Prashanth | Change Password

--- services/user.js
+++ services/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 const DbxService = require('../services/dropbox')
-
+const AuthService = require('./auth')
 
 exports.getUser = async (email) => {
     const user = await User.findOne({ email: email }, { password: 0 })
@@ -15,4 +15,22 @@ exports.updateImage = async (email, image) => {
             image: image
         }
     })
+}
+
+
+exports.updatePassword = async (email, oldPassword, newPassword) => {
+    try {
+        const user = await User.findOne({ email: email })
+        await AuthService.verifyPass(oldPassword, user.password)
+        const hashPass = await AuthService.getHashPassword(newPassword)
+        await User.updateOne({ email: email }, {
+            $set: {
+                password: hashPass
+            }
+        })
+    } catch (err) {
+        console.log(err)
+        throw `Unauthorized`
+    }
+
 }
